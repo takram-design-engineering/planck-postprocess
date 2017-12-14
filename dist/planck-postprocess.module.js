3467c208f4effb49800b910a021d0ae1300f25f3
Shota Matsuda <shota@takram.com>
2017-12-14 12:50:01 +0900

    exposed scissor passes

--- dist/planck-postprocess.module.js
+++ dist/planck-postprocess.module.js
@@ -23,8 +23,10 @@
 //
 
 export { default as BloomPass } from '../src/BloomPass'
+export { default as ClearScissorPass } from '../src/ClearScissorPass'
 export { default as FXAAPass } from '../src/FXAAPass'
 export { default as Postprocess } from '../src/Postprocess'
 export { default as RenderPass } from '../src/RenderPass'
+export { default as ScissorPass } from '../src/ScissorPass'
 export { default as TiltShiftPass } from '../src/TiltShiftPass'
 export { default as VignettePass } from '../src/VignettePass'
