6a01970774748d00d25c97e6ccd3702cbf2f627d
Shota Matsuda <shota@takram.com>
2017-12-14 18:07:02 +0900

    [breaking] use write buffer to render the separate scene to

--- src/BloomPass.js
+++ src/BloomPass.js
@@ -29,21 +29,25 @@ import 'three/examples/js/shaders/LuminosityHighPassShader'
 import 'three/examples/js/postprocessing/UnrealBloomPass'
 
 export default class BloomPass extends Three.UnrealBloomPass {
-  constructor(width, height, ...rest) {
-    super(new Three.Vector2(width, height), ...rest)
-
-    // Additional states to support separate read buffer
-    this.readBuffer = null
+  constructor(width, height, {
+    strength = 1,
+    radius = 0.5,
+    threshold = 0.5,
+  } = {}) {
+    const resolution = new Three.Vector2(width || 256, height || 256)
+    super(resolution, strength, radius, threshold)
     this.needsSeparateRender = false
+    this.separateCamera = null
+    this.separateScene = null
     this.layers = new Three.Layers()
   }
 
   render(renderer, writeBuffer, readBuffer, delta, maskActive) {
-    this.oldClearColor.copy(renderer.getClearColor())
-    this.oldClearAlpha = renderer.getClearAlpha()
-    const oldAutoClear = renderer.autoClear
+    const { autoClear } = renderer
     // eslint-disable-next-line no-param-reassign
     renderer.autoClear = false
+    const clearColor = renderer.getClearColor().getHex()
+    const clearAlpha = renderer.getClearAlpha()
     renderer.setClearColor(new Three.Color(0, 0, 0), 0)
     if (maskActive) {
       renderer.context.disable(renderer.context.STENCIL_TEST)
@@ -51,7 +55,16 @@ export default class BloomPass extends Three.UnrealBloomPass {
 
     // 1. Extract Bright Areas
     if (this.needsSeparateRender) {
-      this.highPassUniforms.tDiffuse.value = this.readBuffer.texture
+      // Use the write buffer to render the separate scene to
+      const {
+        separateCamera: camera,
+        separateScene: scene,
+      } = this
+      const { layers } = camera
+      camera.layers = this.layers
+      renderer.render(scene, camera, writeBuffer, true)
+      camera.layers = layers
+      this.highPassUniforms.tDiffuse.value = writeBuffer.texture
     } else {
       this.highPassUniforms.tDiffuse.value = readBuffer.texture
     }
@@ -59,40 +72,47 @@ export default class BloomPass extends Three.UnrealBloomPass {
     this.quad.material = this.materialHighPassFilter
     renderer.render(this.scene, this.camera, this.renderTargetBright, true)
 
-    // 2. Blur All the mips progressively
-    let inputRenderTarget = this.renderTargetBright
+    // 2. Blur all the mips progressively
+    let renderTarget = this.renderTargetBright
     for (let i = 0; i < this.nMips; ++i) {
       const material = this.separableBlurMaterials[i]
-      const horizontalTarget = this.renderTargetsHorizontal[i]
-      const verticalTarget = this.renderTargetsVertical[i]
+      const horizontalRenderTarget = this.renderTargetsHorizontal[i]
+      const verticalRenderTarget = this.renderTargetsVertical[i]
       this.quad.material = material
-      material.uniforms.colorTexture.value = inputRenderTarget.texture
-      material.uniforms.direction.value = Three.UnrealBloomPass.BlurDirectionX
-      renderer.render(this.scene, this.camera, horizontalTarget, true)
-      material.uniforms.colorTexture.value = horizontalTarget.texture
-      material.uniforms.direction.value = Three.UnrealBloomPass.BlurDirectionY
-      renderer.render(this.scene, this.camera, verticalTarget, true)
-      inputRenderTarget = verticalTarget
+      material.uniforms.colorTexture.value = renderTarget.texture
+      material.uniforms.direction.value = this.constructor.BlurDirectionX
+      renderer.render(this.scene, this.camera, horizontalRenderTarget, true)
+      material.uniforms.colorTexture.value = horizontalRenderTarget.texture
+      material.uniforms.direction.value = this.constructor.BlurDirectionY
+      renderer.render(this.scene, this.camera, verticalRenderTarget, true)
+      renderTarget = verticalRenderTarget
     }
 
-    // Composite All the mips
-    const horizontalTarget = this.renderTargetsHorizontal[0]
+    // Composite all the mips
+    const horizontalRenderTarget = this.renderTargetsHorizontal[0]
     this.quad.material = this.compositeMaterial
     this.compositeMaterial.uniforms.bloomStrength.value = this.strength
     this.compositeMaterial.uniforms.bloomRadius.value = this.radius
     this.compositeMaterial.uniforms.bloomTintColors.value = this.bloomTintColors
-    renderer.render(this.scene, this.camera, horizontalTarget, true)
+    renderer.render(this.scene, this.camera, horizontalRenderTarget, true)
 
     // Blend it additively over the input texture
     this.quad.material = this.materialCopy
-    this.copyUniforms.tDiffuse.value = horizontalTarget.texture
+    this.copyUniforms.tDiffuse.value = horizontalRenderTarget.texture
 
     if (maskActive) {
       renderer.context.enable(renderer.context.STENCIL_TEST)
     }
-    renderer.render(this.scene, this.camera, readBuffer, false)
-    renderer.setClearColor(this.oldClearColor, this.oldClearAlpha)
+    if (this.renderToScreen) {
+      renderer.render(this.scene, this.camera, undefined, false)
+    } else {
+      renderer.render(this.scene, this.camera, readBuffer, false)
+    }
+
     // eslint-disable-next-line no-param-reassign
-    renderer.autoClear = oldAutoClear
+    renderer.autoClear = autoClear
+    if (this.clearColor) {
+      renderer.setClearColor(clearColor, clearAlpha)
+    }
   }
 }
