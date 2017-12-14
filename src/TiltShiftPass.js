0fe100a529b071055fa81dbe5d70205e2691bf29
Shota Matsuda <shota@takram.com>
2017-12-14 13:06:34 +0900

    made tilt pass a single pass (in js code)

--- src/TiltShiftPass.js
+++ src/TiltShiftPass.js
@@ -24,44 +24,89 @@
 
 import * as Three from 'three'
 
-import Namespace from '@takram/planck-core/src/Namespace'
-
 import 'three/examples/js/postprocessing/EffectComposer'
-import 'three/examples/js/postprocessing/ShaderPass'
 
-export const internal = Namespace('TiltShiftPass')
+import fragmentShader from './shader/tilt_shift_frag.glsl'
+import vertexShader from './shader/tilt_shift_vert.glsl'
 
-export default class TiltShiftPass extends Three.ShaderPass {
-  constructor(shader, { amount = 9, center = 0 } = {}) {
-    super(shader)
-    this.denominator = 1000
-    this.amount = amount
-    this.center = center
+export default class TiltShiftPass extends Three.Pass {
+  constructor(width, height, pixelRatio = 1, size = 9, {
+    center = 0,
+    radius = 3,
+    radiusLimit,
+  } = {}) {
+    super()
+    this.uniforms = {
+      tDiffuse: { value: null },
+      resolution: { value: new Three.Vector2(width || 256, height || 256) },
+      direction: { value: new Three.Vector2() },
+      center: { value: 0 },
+      radius: { value: radius },
+      radiusLimit: {
+        value: (radiusLimit !== undefined ? radiusLimit : radius * 2),
+      },
+    }
+    this.needsSwap = false
+    this.camera = new Three.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    this.scene = new Three.Scene()
+    this.material = new Three.ShaderMaterial({
+      defines: {
+        KERNEL_SIZE: size,
+        RESOLUTION: 1024,
+      },
+      uniforms: this.uniforms,
+      vertexShader,
+      fragmentShader,
+    })
+    const geometry = new Three.PlaneBufferGeometry(2, 2)
+    this.quad = new Three.Mesh(geometry, this.material)
+    this.quad.frustumCulled = false
+    this.scene.add(this.quad)
   }
 
-  setSize(width, height) {
-    this.uniforms.amount.value = this.amount / this.denominator
+  render(renderer, writeBuffer, readBuffer, delta, maskActive) {
+    const {
+      scene,
+      camera,
+      material,
+    } = this
+    material.uniforms.tDiffuse.value = readBuffer
+    material.uniforms.direction.value.set(1, 0)
+    renderer.render(scene, camera, writeBuffer, this.clear)
+    material.uniforms.tDiffuse.value = writeBuffer
+    material.uniforms.direction.value.set(0, 1)
+    if (this.renderToScreen) {
+      renderer.render(scene, camera, undefined, this.clear)
+    } else {
+      renderer.render(scene, camera, readBuffer, this.clear)
+    }
   }
 
-  get amount() {
-    const scope = internal(this)
-    return scope.amount
-  }
-
-  set amount(value) {
-    const scope = internal(this)
-    scope.amount = value
-    this.uniforms.amount.value = value / this.denominator
+  setSize(width, height, pixelRatio = 1) {
+    this.uniforms.resolution.value.set(width, height)
   }
 
   get center() {
-    const scope = internal(this)
-    return scope.center
+    return this.uniforms.center.value
   }
 
   set center(value) {
-    const scope = internal(this)
-    scope.center = value
-    this.uniforms.center.value = (value + 1) / 2
+    this.uniforms.center.value = value
+  }
+
+  get radius() {
+    return this.uniforms.radius.value
+  }
+
+  set radius(value) {
+    this.uniforms.radius.value = value
+  }
+
+  get radiusLimit() {
+    return this.uniforms.radiusLimit.value
+  }
+
+  set radiusLimit(value) {
+    this.uniforms.radiusLimit.value = value
   }
 }
---
+++ src/shader/tilt_shift_frag.glsl
@@ -0,0 +1,56 @@
+// The MIT License
+// Copyright (C) 2016-Present Shota Matsuda
+
+#ifndef KERNEL_SIZE
+  #define KERNEL_SIZE 9
+#endif
+
+uniform sampler2D tDiffuse;
+uniform vec2 resolution;
+uniform vec2 direction;
+uniform float center;
+uniform float radius;
+uniform float radiusLimit;
+
+varying vec2 vUv;
+
+void main() {
+  vec4 color = vec4(0.0);
+  float gradient = pow(abs((center * 0.5 + 0.5) - vUv.y) * 2.0, 2.0);
+  vec2 aspect = vec2(1.0, resolution.x / resolution.y);
+  float size = max(resolution.x, resolution.y);
+  vec2 amount = clamp(radius * gradient / size * aspect, 0.0, radiusLimit);
+  vec2 offset = amount * direction;
+
+  #if KERNEL_SIZE == 9
+    color += texture2D(tDiffuse, vUv) * 0.162443;
+    color += texture2D(tDiffuse, vUv + offset) * 0.151793;
+    color += texture2D(tDiffuse, vUv - offset) * 0.151793;
+    color += texture2D(tDiffuse, vUv + offset * 2.0) * 0.123853;
+    color += texture2D(tDiffuse, vUv - offset * 2.0) * 0.123853;
+    color += texture2D(tDiffuse, vUv + offset * 3.0) * 0.08824;
+    color += texture2D(tDiffuse, vUv - offset * 3.0) * 0.08824;
+    color += texture2D(tDiffuse, vUv + offset * 4.0) * 0.0548925;
+    color += texture2D(tDiffuse, vUv - offset * 4.0) * 0.0548925;
+  #endif
+
+  #if KERNEL_SIZE == 7
+    color += texture2D(tDiffuse, vUv) * 0.182476;
+    color += texture2D(tDiffuse, vUv + offset) * 0.170513;
+    color += texture2D(tDiffuse, vUv - offset) * 0.170513;
+    color += texture2D(tDiffuse, vUv + offset * 2.0) * 0.139127;
+    color += texture2D(tDiffuse, vUv - offset * 2.0) * 0.139127;
+    color += texture2D(tDiffuse, vUv + offset * 3.0) * 0.099122;
+    color += texture2D(tDiffuse, vUv - offset * 3.0) * 0.099122;
+  #endif
+
+  #if KERNEL_SIZE == 5
+    color += texture2D(tDiffuse, vUv) * 0.227595;
+    color += texture2D(tDiffuse, vUv + offset) * 0.212674;
+    color += texture2D(tDiffuse, vUv - offset) * 0.212674;
+    color += texture2D(tDiffuse, vUv + offset * 2.0) * 0.1735285;
+    color += texture2D(tDiffuse, vUv - offset * 2.0) * 0.1735285;
+  #endif
+
+  gl_FragColor = color;
+}
